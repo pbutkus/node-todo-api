344ea56a81405fe2a4646ce74be8e083cdcaf6e0
pbutkus <p.butkus360@gmail.com>
2017-09-07 11:39:12 +0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('59b0fef81a32730b4b5be16b')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('59b009b00ee2660b5cd78a25')
+    }, {
+        $set: {
+            name: 'Petras'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
